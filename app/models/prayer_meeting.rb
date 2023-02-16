82c6f6949b88efb058432eab86acc5e42aa6da9f
haochenliu <mmkrty@gmail.com>
2023-02-16 14:32:30 +0100

    add validation for prayer meeting

--- app/models/prayer_meeting.rb
+++ app/models/prayer_meeting.rb
@@ -1,9 +1,14 @@
 class PrayerMeeting < ApplicationRecord
+  validate :validate_date_is_tuesday
   before_save :calculate_total
 
   private
 
   def calculate_total
     self.total = adults + teenagers + children + toddlers
+  end
+
+  def validate_date_is_tuesday
+    errors.add(:date, "must be a Tuesday") unless date.wday == 2
   end
 end
