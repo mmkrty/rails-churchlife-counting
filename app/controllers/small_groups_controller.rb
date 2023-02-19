a900c9e04861e7c60a2c321815d903ccab53e7eb
haochenliu <mmkrty@gmail.com>
2023-02-19 20:19:01 +0100

    add small groups index

--- app/controllers/small_groups_controller.rb
+++ app/controllers/small_groups_controller.rb
@@ -16,6 +16,25 @@ class SmallGroupsController < ApplicationController
     end
   end
 
+  def edit
+    @small_group = SmallGroup.find(params[:id])
+  end
+
+  def update
+    @small_group = SmallGroup.find(params[:id])
+    if @small_group.update(small_group_params)
+      redirect_to small_groups_path, notice: "Small Group updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @small_group = SmallGroup.find(params[:id])
+    @small_group.destroy
+    redirect_to small_groups_path, notice: "Small Group deleted successfully."
+  end
+
   private
 
   def small_group_params
