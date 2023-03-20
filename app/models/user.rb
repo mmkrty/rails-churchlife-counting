ca2eeda3b68f229d01fe867ffda21c772421ef43
haochenliu <mmkrty@gmail.com>
2023-03-20 15:57:18 +0100

    add admin to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessible :admin
+
+  def admin?
+    admin
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
