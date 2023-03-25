d0e03bd56d2b95cbcd43d8f8a22e417bf9c3e618
haochenliu <mmkrty@gmail.com>
2023-03-25 14:23:15 +0100

    change database to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_20_145358) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "lords_days", force: :cascade do |t|
     t.date "date", null: false
     t.string "location", null: false
@@ -21,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_145358) do
     t.integer "teenagers", default: 0
     t.integer "children", default: 0
     t.integer "toddlers", default: 0
-    t.integer "total", default: 0
+    t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -33,7 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_145358) do
     t.integer "teenagers", default: 0
     t.integer "children", default: 0
     t.integer "toddlers", default: 0
-    t.integer "total", default: 0
+    t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -45,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_145358) do
     t.integer "teenagers", default: 0
     t.integer "children", default: 0
     t.integer "toddlers", default: 0
-    t.integer "total", default: 0
+    t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
